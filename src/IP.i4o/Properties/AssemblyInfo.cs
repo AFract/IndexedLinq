97396c2ca699d8616fbda89b391bbd593996ccfa
jkuehner <jochen.kuehner@kardex.com>
2018-07-31 23:36:37 +0200

    bugfix : Create Nuget Package

--- src/IP.i4o/Properties/AssemblyInfo.cs
+++ src/IP.i4o/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNetProjects.IndexedLinq")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +21,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("348933ae-2f07-4ae4-963b-499df2e485e0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("DotNetProjects.IndexedLinq.Tests")]
